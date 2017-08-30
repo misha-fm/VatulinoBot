61cc123a6533ff609b1ce07c6bce8ad2bfc1ccfb
Карпов Михаил <misha_fm@mail.ru>
2017-08-31 00:30:12 +0300

    init

---
+++ BotHandler.py
@@ -0,0 +1,33 @@
+import requests
+
+
+class BotHandler:
+    def __init__(self, token):
+        self.token = token
+        self.api_url = "https://api.telegram.org/bot{}/".format(token)
+        self.received_messages = []
+
+    def get_updates(self, offset=None, timeout=30):
+        method = 'getUpdates'
+        params = {'timeout': timeout, 'offset': offset}
+        resp = requests.get(self.api_url + method, params)
+        result_json = resp.json()['result']
+        self.received_messages.append(result_json)
+        return result_json
+
+    def send_message(self, chat_id, text):
+        params = {'chat_id': chat_id, 'text': text}
+        method = 'sendMessage'
+        resp = requests.post(self.api_url + method, params)
+        return resp
+
+    def get_last_update(self):
+        get_result = self.get_updates()
+        print(len(get_result))
+
+        if len(get_result) > 0:
+            last_update = get_result[-1]
+#        else:
+#            last_update = get_result[len(get_result)]
+
+        return last_update
